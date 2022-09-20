f5b47d437a2007e2b1f84996d67c1c8c4260bf8e
batuctn <batuctnn14@gmail.com>
2022-09-20 09:55:08 +0300

    first commit test2

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>First Commit test1</Text>
+      <Text>First Commit</Text>
       <StatusBar style="auto" />
     </View>
   );
