90c8c0112643d3a7d13d69ccaabd9f7717d3a555
batuctn <batuctnn14@gmail.com>
2022-09-24 03:41:34 +0300

    theme screen nad styles add.

--- src/screens/Theme/Theme.js
+++ src/screens/Theme/Theme.js
@@ -1,12 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import { useDispatch, useSelector } from 'react-redux';
+
+import styles from '../Theme/ThemeStyle';
+import { toggleTheme } from '../../store/index';
 
 const Theme = () => {
-  return (
-    <View>
-      <Text>Theme</Text>
-    </View>
-  )
-}
+  const [isActive, setIsActive] = useState(false);
+  const theme = useSelector((state) => state.theme.activeTheme);
+  const dispatch = useDispatch();
 
-export default Theme
+  console.log(theme);
+  return (
+    <View style={theme === 'light' ? styles.container_light : styles.container_dark}>
+      <TouchableOpacity
+        onPress={() => {
+          setIsActive(!isActive);
+          dispatch(toggleTheme());
+        }}>
+        <View style={styles.iconContain}>
+          <View style={styles.icon}>
+            {isActive ? (
+              <Feather name="sun" size={80} color="black" />
+            ) : (
+              <Fontisto name="night-clear" size={80} color="black" />
+            )}
+          </View>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Theme;
---
+++ src/screens/Theme/ThemeStyle.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+export default StyleSheet.create({
+  container_light: {
+    flexDirection: 'row',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container_dark: {
+    flexDirection: 'row',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    backgroundColor: '#363636',
+    alignItems: 'center',
+  },
+  iconContain: {
+    backgroundColor: '#e3e3e3',
+    height: 150,
+    width: 150,
+    borderRadius: 50,
+    margin: 10,
+  },
+  icon: {
+    marginTop: 35,
+    alignItems: 'center',
+  },
+});
