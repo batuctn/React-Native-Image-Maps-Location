1a0d0344581780833361a18fbc7fc4127c918a2d
batuctn <batuctnn14@gmail.com>
2022-09-24 03:45:08 +0300

    firebase and redux add on screens

--- src/screens/Map/Map.js
+++ src/screens/Map/Map.js
@@ -1,12 +1,79 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, StyleSheet, Alert, Image, Pressable, Button, ImageBackground } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
+import MapView, {Marker} from 'react-native-maps';
+import { Dimensions } from 'react-native';
+import * as Location from 'expo-location';
+import {db} from '../../utils/firebase';
+import {
+  doc,
+  getDocs,
+  query,
+  updateDoc,
+  collection,
+  where,
+} from 'firebase/firestore';
+import {useSelector} from 'react-redux';
+import CustomMarker from '../../components/CustomMarker';
+import {useIsFocused} from '@react-navigation/native';
+
 
 const Map = () => {
+
+  const mapRef = useRef()
+  const user = useSelector(state => state.user.user);
+  const [users, setUsers] = useState([]);
+  const isFocused = useIsFocused();
+
+
+  const getUsers = async () => {
+    const q = query(collection(db, 'user'));
+    const result = await getDocs(q).then(res => {
+      const _users = res.docs.map(item => item.data());
+      setUsers(_users);
+    });
+  };
+  useEffect(() => {
+    getUsers();
+  }, [isFocused]);
+ 
   return (
     <View>
-      <Text>Map</Text>
-    </View>
+       
+      <MapView 
+       initialRegion={{
+        latitude: user.latitude,
+        longitude: user.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+      showsUserLocation 
+      ref={mapRef} 
+      style={styles.map } 
+      maxZoomLevel={20} 
+      minZoomLevel={5} >
+           {users?.map((userItem,index) => {
+          return (
+            <View key={index}>
+            <CustomMarker
+              user={userItem}
+              lat={userItem.latitude}
+              long={userItem.longitude}
+              uri={userItem.photoURL}
+            />
+            </View>
+          );
+        })}
+      </MapView>
+      </View>
+   
   )
 }
 
-export default Map
+export default Map;
+
+const styles = StyleSheet.create ({
+  map:{
+    width:Dimensions.get('screen').width,
+    height:Dimensions.get('screen').height
+  }
+})
--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -1,10 +1,52 @@
-import { View, Text } from 'react-native'
+import { View, Button, Image, Pressable } from 'react-native'
 import React from 'react'
+import { useDispatch, useSelector} from 'react-redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { logout } from '../../store/index';
+import * as ImagePicker from 'expo-image-picker';
+import {doc, updateDoc} from 'firebase/firestore';
+import {db} from '../../utils/firebase';
 
-const Profile = () => {
+import {updateUser} from '../../store/index';
+import CustomBotton from '../../components/CustomBotton';
+import UploadImageAsync from '../../components/UploadImageAsync';
+
+const Profile = ({navigation}) => {
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.user.user);
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!result.cancelled) {
+      const ProfilePhotoUrl = await UploadImageAsync({uri:result.uri});
+          const docRef = doc(db, 'user', user.id);
+          await updateDoc(docRef, {
+            ProfilePhotoUrl:ProfilePhotoUrl
+          }).then(response => {
+            dispatch(updateUser({ProfilePhotoUrl: ProfilePhotoUrl}));
+          });      
+    }
+  };
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={{justifyContent:"center",alignContent:"center",flex:1,}}>
+      <Pressable onPress={pickImage}>
+        <Image source={{uri:user.ProfilePhotoUrl}} style={{width:100,height:100,borderRadius:50,alignSelf:"center",backgroundColor: "gray"}} />
+      </Pressable>
+      <CustomBotton title='Edit Profile' onPress={()=>{
+        navigation.navigate("EditProfile")
+      }}/>
+      <CustomBotton  title='Theme' onPress={()=>{
+        navigation.navigate("Theme")
+      }}/>
+      <CustomBotton title='logout' onPress={async()=>{
+        await AsyncStorage.removeItem('userStorage');
+        dispatch(logout());
+      }} />
     </View>
   )
 }
