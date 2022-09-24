535c35203641c1e5926a0b47fab72259d9cb240d
batuctn <batuctnn14@gmail.com>
2022-09-24 03:44:30 +0300

    auth templated finised

--- src/screens/SignIn/SignIn.js
+++ src/screens/SignIn/SignIn.js
@@ -1,15 +1,120 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react';
 
+import {signInWithEmailAndPassword} from 'firebase/auth';
+import {auth, db} from '../../utils/firebase';
+import {useDispatch} from 'react-redux';
+import {setUser} from '../../store/index';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useNavigation} from '@react-navigation/native';
+import {useForm, Controller} from 'react-hook-form';
+import {Button, Input, Text, View, useToast} from 'native-base';
+import {doc, getDoc} from 'firebase/firestore';
+import CustomBotton from '../../components/CustomBotton';
 
 const SignIn = () => {
- 
- 
-  return (
-    <View >
-      <Text>SignIn</Text>
-    </View>
-  )
-}
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+  const {show} = useToast();
+  const {navigate} = useNavigation();
+  const dispatch = useDispatch();
 
-export default SignIn
+  const handleSignIn = data => {
+    signInWithEmailAndPassword(auth, data.email, data.password)
+      .then(async response => {
+        const userDoc = doc(db, 'user', response.user.uid);
+        const userRef = await getDoc(userDoc);
+        if (userRef.exists()) {
+          AsyncStorage.setItem('userStorage', JSON.stringify(userRef.data()));
+          dispatch(setUser(userRef.data()));
+        }
+      })
+      .catch(err => {
+        show({
+          description: err.message,
+        });
+      });
+  };
+
+  useEffect(() => {
+    AsyncStorage.getItem('userStorage').then((parserespo) => {
+      const parsedata = JSON.parse(parserespo);
+      dispatch(setUser(parsedata));
+    });
+    
+  }, []);
+  return (
+    <View p={4} style={{justifyContent:"center",alignContent:"center",flex:1}}>
+      <Controller 
+        name="email"
+        control={control}
+        rules={{
+
+          required: {value: true, message: 'Email is required'},
+          minLength: {value: 10, message: 'Email can be at least 10 chars. '},
+        }}
+        render={({field}) => {
+          return (
+            <Input
+              w={"90%"}
+              alignSelf="center"
+              placeholder="E-mail"
+              autoCapitalize="none"
+              my={2}
+              {...field}
+              onChangeText={field.onChange}
+            />
+          );
+        }}
+      />
+      {errors.email ? (
+        <Text style={{color: 'red'}}>{errors.email.message}</Text>
+      ) : null}
+
+      <Controller
+        name="password"
+        control={control}
+        render={({field}) => {
+          return (
+            <Input  
+              w={"90%"}
+              alignSelf="center"
+              autoCapitalize="none"
+              secureTextEntry
+              placeholder="Password"
+              my={2}
+              {...field}
+              onChangeText={field.onChange}
+            />
+          );
+        }}
+      />
+
+      <CustomBotton title="Sign In" onPress={handleSubmit(handleSignIn)} />
+
+      <CustomBotton
+        title="Reset Password"
+        variant={'ghost'}
+        onPress={() => {
+          navigate('ForgotPassword');
+        }}/>
+      
+
+      <CustomBotton
+        title="Sign Up"
+        onPress={() => {
+          navigate('SignUp');
+        }} />
+        
+     
+    </View>
+  );
+};
+export default SignIn;
--- src/screens/SignUp/SignUp.js
+++ src/screens/SignUp/SignUp.js
@@ -1,12 +1,70 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {useNavigation} from '@react-navigation/native';
+import {createUserWithEmailAndPassword} from 'firebase/auth';
+import {setDoc, doc} from 'firebase/firestore';
+import {Button, Input, Pressable, Text, View} from 'native-base';
+import {Controller, useForm} from 'react-hook-form';
+import CustomBotton from '../../components/CustomBotton';
+import {auth, db} from '../../utils/firebase';
 
 const SignUp = () => {
-  return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
-  )
-}
+  const {control, handleSubmit} = useForm();
+  const {navigate} = useNavigation();
 
-export default SignUp
+  const handleSignUp = data => {
+    console.log("datas",data);
+    createUserWithEmailAndPassword(auth, data.email, data.password).then(
+      async response => {
+        await setDoc(doc(db, `user`, response.user.uid), {
+          email: response.user.email,
+          id: response.user.uid,
+        });
+        navigate('SignIn');
+      },
+    );
+  };
+
+  return (
+    <View p={4}>
+      <Controller
+        control={control}
+        rules={{
+          required: {value: true, message: 'Email is required'},
+          minLength: {value: 10, message: 'Email can be at least 10 chars. '},
+        }}
+        name="email"
+        render={({field}) => {
+          return (
+            <Input
+              placeholder="E-mail"
+              {...field}
+              my={2}
+              onChangeText={field.onChange}
+            />
+          );
+        }}
+      />
+      <Controller
+        control={control}
+        name="password"
+        rules={{
+          required: {value: true, message: 'Password is required'},
+          minLength: {value: 6, message: 'Password can be at least 6 chars. '},
+        }}
+        render={({field}) => (
+          <Input
+            placeholder="Password"
+            {...field}
+            my={2}
+            onChangeText={field.onChange}
+            secureTextEntry={true}
+          />
+        )}
+      />
+
+      <CustomBotton title="Sign Up" onPress={handleSubmit(handleSignUp)} />
+        
+     
+    </View>
+  );
+};
+export default SignUp;
